5b00e21f2cddb4beb1159fc59d8522f7729dbea0
Bruno Miguel <up201502960@fe.up.pt>
2018-12-01 00:32:56 +0000

    batch_run boolean

--- firefighting/src/firefighting/utils/JADELauncher.java
+++ firefighting/src/firefighting/utils/JADELauncher.java
@@ -29,6 +29,9 @@ import jade.wrapper.StaleProxyException;
  * Class responsible for running and controlling JADE execution.
  */
 public class JADELauncher {	
+	/* Manual or random configuration */
+	static boolean batch_run = false;
+	
 	static Random random = new Random();
 
 	// GUI related stuff
@@ -47,6 +50,54 @@ public class JADELauncher {
 	public static void main(String[] args) throws ControllerException, IOException {
 		new Logger();
 		
+		if(batch_run) {
+			normalRun();
+		}
+		else {
+			batchRun();
+		}
+	}
+
+	private static void batchRun() {
+		Random random = new Random();
+		
+        Config.GRID_HEIGHT = random.nextInt(10) + 1;
+        Config.GRID_WIDTH = random.nextInt(10) + 1;
+        Config.NUM_MAX_WATER_RESOURCES = random.nextInt(10) + 1;
+        Config.NUM_MAX_AIRCRAFTS = random.nextInt(10) + 1;
+        Config.NUM_MAX_FIRES = random.nextInt(10) + 1;
+        
+		worldAgent = new WorldAgent(seasonTypeID, windTypeID);
+
+		gui = new GUI(worldAgent);
+		gui.getFrame().setVisible(true);
+		
+		Runtime rt = Runtime.instance();
+		
+		Profile profile = new ProfileImpl();
+		ContainerController mainContainer = rt.createMainContainer(profile);
+		
+		try {
+			mainContainer.acceptNewAgent("WorldAgent", worldAgent);
+			mainContainer.getAgent("WorldAgent").start();
+			
+			mainContainer.acceptNewAgent("FireStation", worldAgent.getFireStationAgent());
+			mainContainer.getAgent("FireStation").start();
+			
+			for(int i = 0; i < worldAgent.getNumAircraftsAgents(); i++) {
+				mainContainer.acceptNewAgent("AircraftAgent" + i, worldAgent.getAircraftAgents()[i]);
+				mainContainer.getAgent(worldAgent.getAircraftAgents()[i].getLocalName()).start();
+			}
+		}
+		catch (StaleProxyException e1) {
+			e1.printStackTrace();
+		} catch (ControllerException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	};
+	
+	private static void normalRun() {
 		// GUI related stuff
 		welcomeScreen = new WelcomeScreen();
 		welcomeScreen.getFrame().setVisible(true);
